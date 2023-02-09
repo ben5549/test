61f648d43451e64cf870f3ed30f4f039cedbeb19
ben <benr33511@gmail.com>
2023-02-09 12:00:06 +0000

    dsfsudghsudhushguhduhsuhfuhdu

--- Task3b.py
+++ Task3b.py
@@ -4,10 +4,11 @@ def getLetterScore(aLetter):
     try:
         check = False
         aLetter = aLetter.upper()
-        for key, value in Task1.scores_dict.items():
+        for key, value in Task1b.scores_dict.items():
             if key == aLetter:
                 check = True
                 return (value)
     except:
         print(0)
         print("Make sure have entered a single alphabet and as a string")
+
